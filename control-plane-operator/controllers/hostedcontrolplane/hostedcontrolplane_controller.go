dae14c79ea22f4f2b15e3ad60c172f17ee4bf5a9
SANJEEV-Choubey <sanjeev.choubey@ibm.com>
2021-10-14 09:31:33 +0530

    Refactor changes

--- control-plane-operator/controllers/hostedcontrolplane/hostedcontrolplane_controller.go
+++ control-plane-operator/controllers/hostedcontrolplane/hostedcontrolplane_controller.go
@@ -86,9 +86,6 @@ var (
 	)
 )
 
-// NoopReconcile is just a default mutation function that does nothing.
-var NoopReconcile controllerutil.MutateFn = func() error { return nil }
-
 type InfrastructureStatus struct {
 	APIHost                 string
 	APIPort                 int32
